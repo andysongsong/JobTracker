9cd20d8ff4ac2b00d9769429a6cdc9a42f5a4b47
andysongsong <sxj930916@gmai.com>
2022-03-13 21:29:40 -0700

    add landing page

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,31 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+
+const landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobber" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* {info} */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span>app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            placeat harum eius et aspernatur esse voluptatem ab odio nobis?
+            Repudiandae, ab sunt. Eaque dignissimos mollitia consequatur, odit
+            neque vitae cumque.
+          </p>
+          <button className="btn btn-hero">login/register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default landing;
